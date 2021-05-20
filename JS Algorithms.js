fa4190f25a0e46b321a227c770220e390112cbb4
surveyormadu <72581204+surveyormadu@users.noreply.github.com>
2021-05-20 23:50:34 +0100

    Frontend JavaScript Assignment

--- JS Algorithms.js
+++ JS Algorithms.js
@@ -2,97 +2,79 @@
 
 //Convert Fahrenheit To Celsius
 function convertFahrToCelsius(value) {
+	var result;
 
-    var result;
+	if (typeof value === "number" && isNaN(value) === false) {
+		result = convertTemp(value);
+	} else if (typeof value === "string" && isNaN(parseInt(value)) === false && Number(value) === 0) {
+		result = convertTemp(value);
+	} else if (
+		typeof value === "string" &&
+		isNaN(parseInt(value)) === false &&
+		isNaN(Number(value)) === false
+	) {
+		result = convertTemp(value);
+	} else {
+		result =
+			JSON.stringify(value) +
+			" is not a valid number but a/an " +
+			Object.prototype.toString.call(value).slice(8, -1).toLowerCase() +
+			".";
+	}
 
-    if ((typeof value === "number") && (isNaN(value) === false)) {
-        result = convertTemp(value);
-    }
-
-    else if ((typeof value === "string") && (isNaN(parseInt(value)) === false) && (Number(value) === 0)) {
-        result = convertTemp(value);
-    }
-
-    else if ((typeof value === "string") && (isNaN(parseInt(value)) === false) && (isNaN(Number(value)) === false)) {
-        result = convertTemp(value);
-    }
-
-    else {
-        result = JSON.stringify(value) + " is not a valid number but a/an " +
-            Object.prototype.toString.call(value).slice(8, -1).toLowerCase() + ".";
-    };
-
-    console.log(result);
-    return result;
-
-};
+	console.log(result);
+	return result;
+}
 
 function convertTemp(entry) {
-    var degreeCel = (entry - 32) * (5 / 9);
-    return degreeCel.toFixed(4);
-};
-
-
+	var degreeCel = (entry - 32) * (5 / 9);
+	return degreeCel.toFixed(4);
+}
 
 //Check YuGiOh
 function checkYuGiOh(n) {
+	var newArray;
 
-    var newArray;
+	if (typeof n === "number" && isNaN(n) === false && n !== Infinity) {
+		newArray = createArray(n);
+	} else if (typeof n === "string" && isNaN(parseInt(n)) === false && Number(n) === 0) {
+		newArray = createArray(n);
+	} else if (typeof n === "string" && isNaN(parseInt(n)) === false && isNaN(Number(n)) === false) {
+		newArray = createArray(n);
+	} else {
+		newArray = "invalid parameter: " + JSON.stringify(n);
+	}
 
-    if ((typeof n === "number") && (isNaN(n) === false) && (n !== Infinity)) {
-        newArray = createArray(n);
-    }
-
-    else if ((typeof n === "string") && (isNaN(parseInt(n)) === false) && (Number(n) === 0)) {
-        newArray = createArray(n);
-    }
-
-    else if ((typeof n === "string") && (isNaN(parseInt(n)) === false) && (isNaN(Number(n)) === false)) {
-        newArray = createArray(n);
-    }
-
-    else {
-        newArray = "invalid parameter: " + JSON.stringify(n);
-    };
-
-    console.log(newArray);
-    return newArray;
-
-};
-
+	console.log(newArray);
+	return newArray;
+}
 
 function createArray(entry) {
-    var numberArray = [];
-    var yuGiOhArray;
+	var numberArray = [];
+	var yuGiOhArray;
 
-    for (var i = 1; i <= entry; i++) {
-        yuGiOhArray = [];
+	for (var i = 1; i <= entry; i++) {
+		yuGiOhArray = [];
 
-        if (i % 2 === 0) {
-            yuGiOhArray.push("yu");
-        };
+		if (i % 2 === 0) {
+			yuGiOhArray.push("yu");
+		}
 
-        if (i % 3 === 0) {
-            yuGiOhArray.push("gi");
-        };
+		if (i % 3 === 0) {
+			yuGiOhArray.push("gi");
+		}
 
-        if (i % 5 === 0) {
-            yuGiOhArray.push("oh");
-        };
+		if (i % 5 === 0) {
+			yuGiOhArray.push("oh");
+		}
 
-        if (yuGiOhArray.length > 0) {
-           var yuGiOhString = yuGiOhArray.join("-");
-           numberArray.push(yuGiOhString);
-        }
+		if (yuGiOhArray.length > 0) {
+			var yuGiOhString = yuGiOhArray.join("-");
+			numberArray.push(yuGiOhString);
+		} else {
+			numberArray.push(i);
+		}
+	}
 
-        else {
-           numberArray.push(i);
-        };
-
-    };
-
-    return numberArray;
-};
-
-
-
+	return numberArray;
+}
